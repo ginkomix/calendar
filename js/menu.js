3d85b7725e1f8353c86d86112c456b1a2224a2d3
ginkomix <accer96226@gmail.com>
2017-12-06 21:57:50 +0300

    blocChange

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+handleUrl(window.location.href); 
+
+document.querySelector('.menu').addEventListener('click',function(event){
+	if(event.target.tagName!='A') {
+		return;
+	}
+	event.preventDefault();
+	let url = event.target.getAttribute('href');
+	window.location.hash = url;	
+});
+
+function handleUrl(url) {
+	let active = document.querySelector('a.active');
+	let noActive = document.querySelector('a[href="'+url.split('#').pop()+'"]');
+
+	document.querySelector('#'+active.getAttribute('href')).style.display='none';
+	document.querySelector('#'+noActive.getAttribute('href')).style.display='block';
+	active.classList.remove('active');
+	noActive.className = 'active';
+
+
+}
+window.addEventListener('hashchange',function(ev){
+	handleUrl(ev.newURL);
+})
