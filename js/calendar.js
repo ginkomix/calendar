a6bbc09cb19a52e82346087dc6dfdebcdd8c9278
ginkomix <accer96226@gmail.com>
2017-12-11 23:34:07 +0300

    блэт

--- js/calendar.js
+++ js/calendar.js
@@ -1,56 +1,48 @@
-function drawInteractiveCalendar(el,yearParam,monthParam) {
+'use strict';
 
-	var year =yearParam,
-		month = monthParam;
-	var calendar = document.createElement('div');
-	calendar.id = 'calendar';
-	var buttonLeft = document.createElement('button');
-	buttonLeft.innerHTML = '[<]';
-	buttonLeft.id = 'buttonLeft';
-	var data = document.createElement('span');
-	data.className = 'data';
-	var buttonRight = document.createElement('button');
-	buttonRight.innerHTML = '[>]';
-	buttonRight.id = 'buttonRight';
-	var divButton = document.createElement('div');
-	divButton.className = 'divButton';
-	var divButton = document.createElement('div');
-	divButton.className = 'divButton';
-	var boxSave = document.createElement('div');
-	boxSave.className = 'boxSave';
-	var divCalendarMain =document.createElement('div');
-	divCalendarMain.id = 'divCalendarMain';
-	divButton.appendChild(buttonLeft);
-	divButton.appendChild(data);
-	divButton.appendChild(buttonRight);
-	calendar.appendChild(divButton);
-	el.appendChild(calendar);
-	el.appendChild(divCalendarMain);
-	el.appendChild(boxSave);
-	data.innerHTML = year+' '+month;
-	drawCalendar(divCalendarMain,year,month);
-getLocal() ;
 
-	document.getElementById('buttonLeft').onclick = function(e) {
-		month--;
-		if(month===0) {
-			month=12;
-			year--;
+function Calendar(yearParam,monthParam) {
+	let now = new Date();
+	this.year =	yearParam || now.getYear();
+	this.month = monthParam || now.getMonth();
+	this.element = now.getMonth()+now.getMilliseconds();
+	let self = this;
+	this.daySelect = 1;
+
+
+
+	function changeMonthBack(){
+		self.month--;
+		if(self.month===0) {
+			self.month=12;
+			self.year--;
 		}
-		drawCalendar(divCalendarMain,year,month);
-		data.innerHTML = year+' '+month;
 	}
 
-	document.getElementById('buttonRight').onclick = function(e) {
-		month++;
-		if(month===13) {
-			month=1;
-			year++;
+	function changeMonthNext(){
+		self.month++;
+		if(self.month===13) {
+			self.month=1;
+			self.year++;
 		}
-		drawCalendar(divCalendarMain,year,month);
-		data.innerHTML = year+' '+month;
 	}
-	document.getElementById('divCalendarMain').onclick = function(event){
+
+	this.clickChangeCalendar = function(event){
+		if(event.target.tagName!="BUTTON") {
+			return;
+		}
+		let classButton = event.target.getAttribute('class');
+		if(classButton==='buttonLeft'){
+			changeMonthBack();
+			self.drawCalendar();
+		}
+		if(classButton==='buttonRight'){
+			changeMonthNext();
+			self.drawCalendar();
+		}
+	}
+
+	function clickSave(event){
 		var target = event.target;
 		if(target.tagName!='TD') {
 			return;
@@ -60,51 +52,141 @@ getLocal() ;
 		p.innerHTML = target.innerHTML+' '+year+' '+month;
 		localStorage.setItem(target.innerHTML+' '+year+' '+month,target.innerHTML+' '+year+' '+month)
 	}
+
 	function getLocal() {
 		for( var i = 0;i<localStorage.length;i++) {
 			var key = localStorage.key(i);
-
-
 			var p = document.createElement('p');
 			boxSave.appendChild(p);
 			p.innerHTML = localStorage.getItem(key);
 		}
 	}
 
+	this.newTask = function(event) {
+
+		self.getNumDay(event,self.daySelect).then(function(day){
+			self.daySelect = day;
+			self.daySelect = self.setActive(self.daySelect);
+		});
 
 
 
-	function drawCalendar(el,year,month) {
-		el.innerHTML ='';
-		function getDayNumber(date) { 
-			var number = date.getDay();
-			if(number === 0)
-			{
-				return number = 6;
-			}
-			else  return number - 1;
+	}
 
+	Calendar.prototype.drawInteractiveCalendar = function() {
+		var el = document.createElement('div');
+		el.id = 'interactiveCalendar'+this.element;
+		document.querySelector('#calendar').appendChild(el);
+
+		var buttonLeft = document.createElement('button');
+		buttonLeft.innerHTML = '[<]';
+		buttonLeft.className = 'buttonLeft';
+		var data = document.createElement('span');
+		data.className = 'data'+this.element;
+		var buttonRight = document.createElement('button');
+		buttonRight.innerHTML = '[>]';
+		buttonRight.className = 'buttonRight';
+		var divButton = document.createElement('div');
+		divButton.className = 'divButton';
+
+		var boxSave = document.createElement('div');
+		boxSave.className = 'boxSave'+this.element;
+		var divCalendarMain =document.createElement('div');
+		divCalendarMain.id = 'divCalendarMain'+this.element;
+		divButton.appendChild(buttonLeft);
+		divButton.appendChild(data);
+		divButton.appendChild(buttonRight);
+		el.appendChild(divButton);
+
+		el.appendChild(divCalendarMain);
+		el.appendChild(boxSave);
+		data.innerHTML = this.year+' '+this.month;
+	}
+
+	Calendar.prototype.getDayNumber = function(date) { 
+		var number = date.getDay();
+		if(number === 0) {
+			return number = 6;
 		}
-		var now = new Date(year,month-1);
+		else  return number - 1;
+	}
 
-		var Calendar = '<table><tr>';
-		for(var i=0;i<getDayNumber(now);i++) {
+	Calendar.prototype.drawCalendar = function() {
+		document.querySelector('#divCalendarMain'+this.element).innerHTML ='';
+		var now = new Date(this.year,this.month-1);
+		var Calendar = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>св</th><tr>';
+		for(var i=0;i<this.getDayNumber(now);i++) {
 			Calendar+='<th></th>';
 		}
-		while(now.getMonth()===month-1) {
-
+		while(now.getMonth()===this.month-1) {
 			Calendar += '<td>' + now.getDate() + '</td>';
-			if (getDayNumber(now) % 6 === 0 && getDayNumber(now)!==0) {
-
+			if (this.getDayNumber(now) % 6 === 0 && this.getDayNumber(now)!==0) {
 				Calendar += '</tr><tr>';
 			}
 			now.setDate(now.getDate() + 1);
 		}
 		Calendar += '</tr></table>';
-		el.innerHTML=  Calendar;
 
+		document.querySelector('#divCalendarMain'+this.element).innerHTML=  Calendar;
+		document.querySelector('.data'+this.element).innerHTML = this.year+' '+this.month;
+		this.daySelect = this.setActive(this.daySelect);
 	}
-}
 
-var calendar = document.getElementById('interactiveCalendar');
-drawInteractiveCalendar(calendar,2017,11);
+	Calendar.prototype.getNumDay = function(event,day) {
+		return new Promise(function(resolve) {
+			var target = event.target;
+			if(target.tagName != 'TD') {
+				resolve( day);
+			}
+			resolve(parseInt(target.innerHTML));
+		});
+	}
+
+	Calendar.prototype.setActive = function(dayActive) {
+		var day = dayActive;
+		var td =	document.querySelectorAll('#divCalendarMain'+this.element+' td');
+		if(day>td.length) {
+			day = 1;
+		}
+		td.forEach(function(item,i,td) {
+			if(item.className === 'activeDay') {
+				item.removeAttribute('class');
+			}
+		});
+		td.forEach(function(item,i,td) {
+			if(parseInt(item.innerHTML) === day) {
+				item.className = 'activeDay';
+			}
+		});
+		return day;	
+	}
+
+	let calendar = new Calendar(2017,12);
+	calendar.drawInteractiveCalendar();
+	calendar.drawCalendar();
+	document.querySelector('.divButton').addEventListener('click',calendar.clickChangeCalendar);
+	document.querySelector('#divCalendarMain'+calendar.element).addEventListener('click',calendar.newTask);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,7 @@ document.querySelector('.menu').addEventListener('click',function(event){
 	if(event.target.tagName!='A') {
 		return;
 	}
+
 	event.preventDefault();
 	let url = event.target.getAttribute('href');
 	window.location.hash = url;	
@@ -11,15 +12,17 @@ document.querySelector('.menu').addEventListener('click',function(event){
 
 function handleUrl(url) {
 	let active = document.querySelector('a.active');
-	let noActive = document.querySelector('a[href="'+url.split('#').pop()+'"]');
 
-	document.querySelector('#'+active.getAttribute('href')).style.display='none';
-	document.querySelector('#'+noActive.getAttribute('href')).style.display='block';
-	active.classList.remove('active');
-	noActive.className = 'active';
-
-
+	if(window.location.hash=='') {
+		window.location.hash =active.getAttribute('href');
+	} else {
+		let noActive = document.querySelector('a[href="'+url.split('#').pop()+'"]');
+		document.querySelector('#'+active.getAttribute('href')).style.display='none';
+		document.querySelector('#'+noActive.getAttribute('href')).style.display='block';
+		active.classList.remove('active');
+		noActive.className = 'active';
+	}
 }
 window.addEventListener('hashchange',function(ev){
 	handleUrl(ev.newURL);
-})
+});
